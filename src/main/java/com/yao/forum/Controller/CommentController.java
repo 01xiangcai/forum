b691176b8c6ca93a0be12eb599fdfd49fc587fb5
姚岳龙 <1442721288@qq.com>
2022-12-15 01:17:01 +0800

    回复问题后端接口

---
+++ src/main/java/com/yao/forum/Controller/CommentController.java
@@ -0,0 +1,33 @@
+package com.yao.forum.Controller;
+
+import com.yao.forum.dto.CommentDTO;
+import com.yao.forum.mapper.CommentMapper;
+import com.yao.forum.model.Comment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+@Controller
+public class CommentController {
+    @Resource
+    private CommentMapper commentMapper;
+
+    @ResponseBody
+    @RequestMapping(value = "/comment", method = RequestMethod.POST)
+    public Object post(@RequestBody CommentDTO commentDTO) {
+        Comment comment = new Comment();
+        comment.setParentId(commentDTO.getParentId());
+        comment.setContent(commentDTO.getContent());
+        comment.setType(commentDTO.getType());
+        comment.setGmtCreate(System.currentTimeMillis());
+        comment.setGmtModified(System.currentTimeMillis());
+        comment.setCommentator(1);
+        commentMapper.insert(comment);
+        return null;
+    }
+
+}
